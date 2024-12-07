e4984f463271e0dfff9475b737efd30c312bb9ec
wasifsomji <wasifsomji@gmail.com>
2024-12-06 21:38:54 -0500

    added logos

--- on-orbit-frontend/src/components/navbar/page.tsx
+++ on-orbit-frontend/src/components/navbar/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { useState } from 'react';
+
+import csaLogo from "@/assets/csa.png";
+import mcgillLogo from "@/assets/mcgill.png";
 
 export default function Navbar() {
     // we want to set this to true when session headers are present (user is logged in)
@@ -10,8 +14,13 @@ export default function Navbar() {
     return (
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-bold">
-                    <Link href="/">On Orbit Space Predictor</Link>
+                <div className="flex items-center space-x-4">
+                    <Image src={csaLogo} alt="CSA Logo" width={80} height={80} />
+                    X
+                    <Image src={mcgillLogo} alt="McGill Logo" width={80} height={80} />
+                    <div className="text-white text-lg font-bold">
+                        <Link href="/">On Orbit Space Predictor</Link>
+                    </div>
                 </div>
                 <div className="space-x-4 flex items-center">
                     <Link href="/" className="text-gray-300 hover:text-white">Home</Link>
