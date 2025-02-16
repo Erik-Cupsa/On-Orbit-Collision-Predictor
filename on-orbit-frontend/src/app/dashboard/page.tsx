d252cf055c5147431d699bd7ffacaf389bd9465a
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-02-16 13:28:20 -0500

    changes to frontend

--- on-orbit-frontend/src/app/dashboard/page.tsx
+++ on-orbit-frontend/src/app/dashboard/page.tsx
@@ -79,29 +79,6 @@ export default function Dashboard() {
     return (
         <div className="flex flex-col h-full bg-white min-h-screen w-screen">
             <Navbar />
-            {/* <main className="w-full max-w-[70%] mx-auto p-4 flex-grow">
-                <section className="my-8">
-                    <h2 className="text-2xl font-bold mb-4 text-black">Dashboard</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <div className="bg-white p-4 rounded shadow">
-                            <h3 className="text-xl font-bold mb-2 text-black">User Information</h3>
-                            <p className="text-gray-700">Name: John Doe</p>
-                            <p className="text-gray-700">Email: john.doe@example.com</p>
-                        </div>
-                        <div className="bg-white p-4 rounded shadow">
-                            <h3 className="text-xl font-bold mb-2 text-black">Recently Viewed CDMs</h3>
-                            <p className="text-gray-700">CDM 1</p>
-                            <p className="text-gray-700">CDM 2</p>
-                            <p className="text-gray-700">CDM 3</p>
-                        </div>
-                        <div className="bg-white p-4 rounded shadow">
-                            <h3 className="text-xl font-bold mb-2 text-black">Saved CDMs</h3>
-                            <p className="text-gray-700">Placeholder for Saved CDMs</p>
-
-                        </div>
-                    </div>
-                </section>
-            </main> */}
             <div className="container mx-auto p-4">
                 <h1 className="text-2xl font-bold mb-4">Conjunction Data Messages (CDMs)</h1>
                 {loading ? (
