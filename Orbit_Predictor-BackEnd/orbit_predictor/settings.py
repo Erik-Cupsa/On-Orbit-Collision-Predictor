47273b160ce40a8d53c50c10a2a2609fce50a333
wasifsomji <wasifsomji@gmail.com>
2025-04-06 12:38:12 -0400

    fix email sending

--- Orbit_Predictor-BackEnd/orbit_predictor/settings.py
+++ Orbit_Predictor-BackEnd/orbit_predictor/settings.py
@@ -141,7 +141,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "").replace('\xa0', '')
 
 
 # CORS configuration: Allow requests from http://localhost:3000
